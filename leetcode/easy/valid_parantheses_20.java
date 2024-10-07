df2df42a9b928ce4dfa4e23dacd01e296d8f7aa9
CH6832 <147909141+CH6832@users.noreply.github.com>
2024-10-07 13:18:41 +0200

    Update valid_parantheses_20.java

--- leetcode/easy/valid_parantheses_20.java
+++ leetcode/easy/valid_parantheses_20.java
@@ -1,1 +1,33 @@
+import java.util.Stack;
+import java.util.HashMap;
 
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> bracketStack = new Stack<>();  // Stack to hold opening brackets
+        HashMap<Character, Character> bracketMap = new HashMap<>();  // Map for closing and opening brackets
+
+        bracketMap.put(')', '(');
+        bracketMap.put('}', '{');
+        bracketMap.put(']', '[');
+
+        for (char c : s.toCharArray()) {  
+            if (bracketMap.containsKey(c)) { 
+                if (bracketStack.isEmpty() || bracketStack.pop() != bracketMap.get(c)) {
+                    return false;
+                }
+            } else {
+                bracketStack.push(c);
+            }
+        }
+        
+        return bracketStack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("()"));
+        System.out.println(solution.isValid("()[]{}"));
+        System.out.println(solution.isValid("(]"));
+        System.out.println(solution.isValid("([])"));
+    }
+}
