5bb3a188e3289f614ca073cda153bead1243de66
CH6832 <147909141+CH6832@users.noreply.github.com>
2024-10-08 10:46:00 +0200

    Create sqrt_x_69.py

---
+++ tech-studio-projects/leetcode/easy/sqrt_x_69.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+Example 1:
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+
+Example 2:
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+Constraints:
+0 <= x <= 231 - 1
+
+Time Complexity:
+O(LogN)
+Space Complexity:
+O(1)
+"""
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+
+        l = 1
+        r = x // 2
+
+        res = 0
+        # The while loop sets the search range here.
+        # If it is l becomes greater then r, then there
+        # is no search range.
+        while l <= r:
+            # m is the middle point for the search.
+            m = (l + r) // 2
+            if m * m == x:
+                # Exact square root is found.
+                return m
+            elif m * m < x:
+                # m is to small to be apotential square root
+                res = m
+                # Focus on larger numbers.
+                l = m + 1
+            else: # (m * m > x)
+                # Focus on smaller numbers.
+                r = m - 1
+
+        return res
+
+sol = Solution()
+print(sol.mySqrt(4))
+print(sol.mySqrt(8))
